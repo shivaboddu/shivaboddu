226f35791f75910a2f44f5c94e6eadc0435fe8f2
bsrk236 <bsrk236@gmail.com>
2022-11-07 07:39:27 +0000

    Fixed the issue of key error with different solutions

--- dockerfile
+++ dockerfile
@@ -1,4 +1,11 @@
 FROM ubuntu:18.04
 RUN apt-get update
-RUN apt-get install -y python
-ENTRYPOINT [ "./test.py" ]
+RUN apt-get install -y python3
+RUN apt-get install -y python3-pip
+RUN pip3 install pyyaml
+RUN ln -s /usr/bin/python3 /usr/bin/python
+ADD config.yaml /config.yaml
+ADD test.py /test.py
+RUN chmod 777 /test.py
+WORKDIR /
+ENTRYPOINT ["./test.py"]
--- test.py
+++ test.py
@@ -1,3 +1,74 @@
+#!/usr/bin/python
 import os
-config_data=os.environ["url_list"]
-print(" configuration data:      ", config_data)
+#import json
+import yaml
+#import time
+#import ast
+
+#from yaml.loader import SafeLoader
+
+'''
+url_list={'KEPI_KEY' : 'https://1', 'KPI_KEY' : 'https://2'}
+print(type(url_list))
+abc=['KEPI_KEY', 'KPI_KEY']
+print(type(abc))
+
+for ele in abc:
+    print('ele: ', ele)
+    print('url_ele', url_list[ele])
+
+
+with open('/config.yaml', 'r') as f:
+    data=yaml.load(f.read(), Loader=SafeLoader)
+
+print(data)
+
+abc=['KEPI_KEY', 'KPI_KEY']
+for ele in abc:
+    print('ele: ', ele)
+    print('url_ele', data["url_list"][ele])
+
+
+#code for json
+#time.sleep(100000)
+print(os.getenv("url_list"))
+
+data=ast.literal_eval(os.getenv("url_list"))
+print(type(data))
+
+print(os.getenv("abc"))
+data1=ast.literal_eval(os.getenv("abc"))
+print(type(data1))
+
+for ele in data:
+    print('ele :', ele)
+    print(data[ele])
+
+for ele in data1:
+    print('ele :', ele)
+    print(ele)
+
+# Code for yaml
+#time.sleep(100000)
+'''
+
+print(os.getenv("url_list"))
+
+config_data=yaml.safe_load(os.getenv("url_list"))
+print(type(config_data))
+print(" configuration data: ", config_data)
+
+filter_list=yaml.safe_load(os.getenv("filter_list"))
+print(type(filter_list))
+print(" \n filter data : ",filter_list)
+
+
+for ele in filter_list:
+    print('ele :', ele)
+    print(config_data[ele])
+'''
+for ele in data1:
+    print('ele :', ele)
+    print(ele)
+#time.sleep(100000)
+'''
